dc157bdb1763db4f93eaeb85d937c113d0d5820e
Nicoore <nicoaleore@gmail.com>
2022-10-14 16:44:10 -0300

    footer content

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,31 +1,17 @@
 import './footer.css'
 import instagram from '../images/Trazado 3.png'
 import twitter from '../images/Trazado 2.png'
-import facebook from '../images/Trazado 1.png'
+import facebook from '../images/facebook.png'
+import mail from '../images/Mail.png'
 
 export function Footer() {
     return(
         <div className='footer'>
             <ul>
-                <li>Footer Menu</li>
-                <li>Footer Menu</li>
-                <li>Footer Menu</li>
-            </ul>
-            <ul>
-                <li>Footer Menu</li>
-                <li>Footer Menu</li>
-                <li>Footer Menu</li>
-            </ul>
-            <ul>
-                <li>Footer Menu</li>
-                <li>Footer Menu</li>
-                <li>Footer Menu</li>
-            </ul>
-
-            <ul className='social__images'>
-                <li><img src={instagram}/></li>
-                <li><img src={twitter}/></li>
-                <li><img src={facebook}/></li>
+                <li><img src={facebook} className='footer__thumbnail'/><p className='footer__text'>Baredes Baires</p></li>
+                <li><img src={mail} className='footer__thumbnail'/><p className='footer__text'>baredesbaires@gmail.com</p></li>
+                <li><p className='footer__text'>Razón social: <b><i>drynks S.A.</i></b></p></li>
+                <li><p className='footer__text'>CUIT: 30639204754</p></li>
             </ul>
         </div>
     )
